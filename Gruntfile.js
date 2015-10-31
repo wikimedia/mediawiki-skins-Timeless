4b7fdbbdd35eabaa709df501c5bf97abb709e66b
Paladox <thomasmulhall410@yahoo.com>
2015-10-31 19:34:51 +0000

    Update Jenkins tests
    Add support for running jshint in npm.
    
    Add support for running tests through composer.json.
    
    Change-Id: I97ab0afa8edaba06821651b595a470b0771d354d

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,29 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
-	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
 
 	grunt.initConfig( {
+		jshint: {
+			all: [
+				'**/*.js',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
 		banana: {
 			all: 'i18n/'
 		},
 		jsonlint: {
 			all: [
 				'**/*.json',
-				'!node_modules/**'
+				'!node_modules/**',
+				'!vendor/**'
 			]
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'jshint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
