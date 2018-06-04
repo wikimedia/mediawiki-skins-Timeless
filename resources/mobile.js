138036882f0132638bc63a9e365578d2b4164c2a
Isarra <zhorishna@gmail.com>
2018-06-04 20:04:59 +0000

    Redo width cutoffs to keep content wider and better match values elsewhere in mw
    * Bump max content width up to 125em because why not
    * Make fixed-width padding a bit less
    * Basically just follow WikimediaUI Base breakpoints as appropriate
      while keeping content similarly wide across devices
    
    Bug: T165945
    Change-Id: I8afb30bc1fb24907f94195b31289a664c050fcc4

--- resources/mobile.js
+++ resources/mobile.js
@@ -1,33 +1,34 @@
 /* Popout menus (header) */
 
 $( function () {
-	var toggleTime = 200;
+	var toggleTime = 200,
+		mobileCutoff = 720;
 
 	// Open the various menus
 	$( '#user-tools h2' ).on( 'click', function () {
-		if ( $( window ).width() < 851 ) {
+		if ( $( window ).width() < mobileCutoff ) {
 			$( '#personal-inner, #menus-cover' ).fadeToggle( toggleTime );
 		}
 	} );
 	$( '#site-navigation h2' ).on( 'click', function () {
-		if ( $( window ).width() < 851 ) {
+		if ( $( window ).width() < mobileCutoff ) {
 			$( '#site-navigation .sidebar-inner, #menus-cover' ).fadeToggle( toggleTime );
 		}
 	} );
 	$( '#site-tools h2' ).on( 'click', function () {
-		if ( $( window ).width() < 851 ) {
+		if ( $( window ).width() < mobileCutoff ) {
 			$( '#site-tools .sidebar-inner, #menus-cover' ).fadeToggle( toggleTime );
 		}
 	} );
 	$( '#ca-more' ).on( 'click', function () {
 		$( '#page-tools .sidebar-inner' ).css( 'top', $( '#ca-more' ).offset().top + 25 );
-		if ( $( window ).width() < 851 ) {
+		if ( $( window ).width() < mobileCutoff ) {
 			$( '#page-tools .sidebar-inner, #menus-cover' ).fadeToggle( toggleTime );
 		}
 	} );
 	$( '#ca-languages' ).on( 'click', function () {
 		$( '#other-languages .sidebar-inner' ).css( 'top', $( '#ca-languages' ).offset().top + 25 );
-		if ( $( window ).width() < 851 ) {
+		if ( $( window ).width() < mobileCutoff ) {
 			$( '#other-languages .sidebar-inner, #menus-cover' ).fadeToggle( toggleTime );
 		}
 	} );
@@ -43,7 +44,7 @@ $( function () {
 
 	// Include alternative closing method for ios
 	$( window ).on( 'swiperight', function () {
-		if ( $( window ).width() < 851 ) {
+		if ( $( window ).width() < mobileCutoff ) {
 			$( '#personal-inner' ).fadeOut( toggleTime );
 			$( '.sidebar-inner' ).fadeOut( toggleTime );
 			$( '#menus-cover' ).fadeOut( toggleTime );
