7f87813bc315667ea19055f4181a0f4380af3939
Fomafix <fomafix@googlemail.com>
2019-08-29 11:19:29 +0200

    Simplify JavaScript code
    * Reuse $() closure.
    * Query jQuery selector only once.
    
    Change-Id: Ic302427fe7d23808b3e6718fd23a6b5d492bfa88

--- resources/main.js
+++ resources/main.js
@@ -1,32 +1,31 @@
-/**
- * Focus on search box when 'Tab' key is pressed once
- */
-$( function () {
-	$( '#searchInput' ).attr( 'tabindex', $( document ).lastTabIndex() + 1 );
-} );
-
-/**
- * Desktop menu click-toggling
- *
- * We're not even checking if it's desktop because the classes in play have no effect
- * on mobile regardless... this may break things at some point, though.
- */
 $( function () {
 	// sidebar-chunk only applies to desktop-small, but the toggles are hidden at
 	// other resolutions regardless and the css overrides any visible effects.
-	var dropdowns = '#personal, #p-variants-desktop, .sidebar-chunk';
+	var $dropdowns = $( '#personal, #p-variants-desktop, .sidebar-chunk' );
+
+	/**
+	 * Focus on search box when 'Tab' key is pressed once
+	 */
+	$( '#searchInput' ).attr( 'tabindex', $( document ).lastTabIndex() + 1 );
+
+	/**
+	 * Desktop menu click-toggling
+	 *
+	 * We're not even checking if it's desktop because the classes in play have no effect
+	 * on mobile regardless... this may break things at some point, though.
+	 */
 
 	/**
 	 * Close all dropdowns
 	 */
 	function closeOpen() {
-		$( dropdowns ).removeClass( 'dropdown-active' );
+		$dropdowns.removeClass( 'dropdown-active' );
 	}
 
 	/**
 	 * Click behaviour
 	 */
-	$( dropdowns ).on( 'click', function ( e ) {
+	$dropdowns.on( 'click', function ( e ) {
 		var wasOpen = false;
 		// Check if it's already open so we don't open it again
 		if ( $( this ).hasClass( 'dropdown-active' ) ) {
@@ -39,7 +38,7 @@ $( function () {
 		}
 	} );
 	$( document ).on( 'click', function ( e ) {
-		if ( $( e.target ).closest( dropdowns ).length > 0 ) {
+		if ( $( e.target ).closest( $dropdowns ).length > 0 ) {
 			// Clicked inside an open menu; don't close anything
 		} else {
 			closeOpen();
